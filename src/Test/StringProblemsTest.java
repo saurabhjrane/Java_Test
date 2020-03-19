df962413037712235e0b8ef8218120d217dda92c
saurabhjrane <60829665+saurabhjrane@users.noreply.github.com>
2020-03-19 10:50:43 +0000

    Update StringProblemsTest.java

--- src/Test/StringProblemsTest.java
+++ src/Test/StringProblemsTest.java
@@ -1,4 +1,4 @@
-package Test;
+ackage Test;
 
 import com.company.StringProblems;
 import org.junit.*;
@@ -12,35 +12,50 @@ public class StringProblemsTest {
     String blankString = "";
 
     @Test
+    public void Test_LongestWord()
+    {
+        StringProblems sp = new StringProblems();
+        String longestWord = sp.LongestWord(sampleString);
+        assertEquals( "jumped",longestWord);
+  }
+
+    @Test
+    public void Test_ShortestWord()
+    {
+        StringProblems sp = new StringProblems();
+        String shortWord = sp.ShortestWord(sampleString);
+        assertEquals("The",shortWord);
+    }
+
+    @Test
+    public void Test_LongestEmpty()
+    {
+        StringProblems sp = new StringProblems();
+        String invalid = sp.LongestWord(blankString);
+        assertEquals( "Invalid Input",invalid);
+    }
+
+    @Test
+    public void Test_ShortEmpty()
+    {
+        StringProblems sp = new StringProblems();
+        String invalid = sp.ShortestWord(blankString);
+        assertEquals( "Invalid Input",invalid);
+    }
+
+    @Test
     public void Test_LongestWordLength()
     {
         StringProblems sp = new StringProblems();
-        String longestWord = sp.LongestWordLength(sampleString);
-        assertEquals( "jumped",longestWord);
-  }
+        int longestWordLen = sp.LongestWordLength(sampleString);
+        assertEquals( 6,longestWordLen);
+    }
+
 
     @Test
     public void Test_ShortestWordLength()
     {
         StringProblems sp = new StringProblems();
-        String shortWord = sp.ShortestWordLength(sampleString);
-        assertEquals("The",shortWord);
+        int shortestWorLen = sp.ShortestWordLength(sampleString);
+        assertEquals( 3,shortestWorLen);
     }
-
-    @Test
-    public void Test_LongestEmptyLength()
-    {
-        StringProblems sp = new StringProblems();
-        String invalid = sp.LongestWordLength(blankString);
-        assertEquals( "Invalid Input",invalid);
-    }
-
-    @Test
-    public void Test_ShortEmptyLength()
-    {
-        StringProblems sp = new StringProblems();
-        String invalid = sp.ShortestWordLength(blankString);
-        assertEquals( "Invalid Input",invalid);
-    }
-
-}
